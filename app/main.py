80377704db03b80e801f163aa66cc1ecc50b01c3
mivallion <mivallion@gmail.com>
2020-11-26 23:39:25 +0300

    WiP

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 from typing import Optional
 
 from fastapi import FastAPI
+
 from pydantic import BaseModel
 
 from pymongo import MongoClient
 
 import datetime
+
+import requests
+
+from asyncscheduler import AsyncScheduler
+
+import json
 
 app = FastAPI()
 
@@ -13,14 +20,46 @@ client = MongoClient('mongodb://root:1234@127.0.0.1:27017/')
 db = client['stat_database']
 query_col = db['query_col']
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+avito_app_key = "ZaeC8aidairahqu2Eeb1quee9einaeFieboocohX"
+
+def get_query_count(query, location_id):
+    r = requests.get("https://www.avito.ru/api/10/items?countOnly=1&locationId={location_id}&query={query}&key={avito_app_key}").json()
+    print(r)
+    if r["status"] == "ok":
+        return r["result"]["count"]
+    else:
+        return None
+
+def update_queries():
+    queries = query_col.find()
+    for query in queries:
+        t = int(datetime.datetime.now().timestamp())
+        if "counts" not in query:
+            query["counts"] = {}
+        query["counts"][t] = get_query_count(query["query"], query["locationId"])
+
+        query_col.update_one({query["query"], query["locationId"]}, {"$set": {"counts": query["counts"]}})
+
+    queries = query_col.find()
+    for query in queries:
+        print(query)
+
+a = AsyncScheduler()
+# a.start()
+# a.repeat(3600, 1, update_queries)
 
 class Query(BaseModel):
     text: str
     locationId: Optional[int] = None
+
+@app.get("/update")
+async def update():
+    update_queries()
+    return {}
+
+@app.get("/stop")
+async def stop():
+    a.stop()
 
 @app.put("/query")
 async def add_query(query: Query):
